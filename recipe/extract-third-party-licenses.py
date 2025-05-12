8219517a21f275a690e60a03b505ce217397c568
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-05-12 15:59:17 -0500

    update license script from alpha

--- recipe/extract-third-party-licenses.py
+++ recipe/extract-third-party-licenses.py
@@ -1,21 +1,21 @@
 """extract the third-party-licenses.json file from the app bundle"""
+
 from pathlib import Path
 import os
 import tarfile
 import sys
 
 SRC_DIR = Path(os.environ["SRC_DIR"])
-JS_VERSION = (
+MOD_SRC = SRC_DIR / "jupyterlite_core"
+JS_VERSION = js_version = (
     os.environ["PKG_VERSION"]
     .replace("a", "-alpha.")
     .replace("b", "-beta.")
     .replace("rc", "-rc.")
 )
-CORE_SRC = SRC_DIR
-MOD_SRC = CORE_SRC / "jupyterlite_core"
-APP_TGZ = MOD_SRC / f"jupyterlite-app-{JS_VERSION}.tgz"
+APP_TGZ = MOD_SRC / f"""jupyterlite-app-{JS_VERSION}.tgz"""
 TPLJ = "third-party-licenses.json"
-CORE_TPLJ = CORE_SRC / TPLJ
+CORE_TPLJ = SRC_DIR / TPLJ
 
 if not APP_TGZ.exists():
     print(f"there is no {APP_TGZ}")
@@ -24,4 +24,4 @@ if not APP_TGZ.exists():
 with tarfile.open(APP_TGZ, mode="r:gz") as tf:
     CORE_TPLJ.write_bytes(tf.extractfile(f"package/build/{TPLJ}").read())
 
-print(f"Extracted {TPLJ} to {CORE_SRC / TPLJ}")
+print(f"Extracted {TPLJ} to {SRC_DIR / TPLJ}")
